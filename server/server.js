68224d9aed600f2089e7687f41ad313ff364b128
MrWooJ <Hurricanc@gmail.com>
2019-04-03 11:50:40 +0430

    Edit Server Script in Order to Fit ES7

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
-'use strict';
+let loopback = require('loopback');
+let boot = require('loopback-boot');
+const { join } = require('path');
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+let app = loopback();
+module.exports = app;
 
-var app = module.exports = loopback();
+let envAttach = require('../helper/envAttach');
+envAttach(app);
+
+global.rootRequire = name => require(join(__dirname, '..', name));
+global.rootPath = join(__dirname, '../');
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    let baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      let explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
@@ -21,9 +27,12 @@ app.start = function() {
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+  if (err) {
+    throw err;
+  }
 
   // start the server if `$ node server.js`
-  if (require.main === module)
+  if (require.main === module) {
     app.start();
+  }
 });
