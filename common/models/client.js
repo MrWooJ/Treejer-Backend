4c21681f79867fd1ff7315617eed79a62993d940
MrWooJ <Hurricanc@gmail.com>
2019-04-04 18:16:21 +0430

    Validate Invitation Properties e.g. Status

--- common/models/client.js
+++ common/models/client.js
@@ -1,4 +1,13 @@
+let app = require('../../server/server');
+
 module.exports = async Client => {
+
+  let statusList = [];
+  for (let key in app.vars.config.invitationStatus) {
+    statusList.push(app.vars.config.invitationStatus[key]);
+	}
+
+	Client.validatesInclusionOf('status', { in: statusList });
 
   require('../client/createLogic')(Client);
   require('../client/changeStatus')(Client);
--- common/models/invitation.js
+++ common/models/invitation.js
@@ -1,4 +1,13 @@
+let app = require('../../server/server');
+
 module.exports = async Invitation => {
+
+  let statusList = [];
+  for (let key in app.vars.config.invitationStatus) {
+    statusList.push(app.vars.config.invitationStatus[key]);
+	}
+
+	Invitation.validatesInclusionOf('status', { in: statusList });
 
   require('../invitation/createLogic')(Invitation);
   require('../invitation/changeStatus')(Invitation);
