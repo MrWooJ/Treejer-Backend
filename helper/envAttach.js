d31aa55e6facda62ed234a45f2afaab8fce39bc2
MrWooJ <Hurricanc@gmail.com>
2019-04-04 15:03:03 +0430

    Add Tree Types Config Model to EnvAttacher Module

--- helper/envAttach.js
+++ helper/envAttach.js
@@ -1,11 +1,13 @@
 const clientStatus = require('../config/client.status.js');
 const invitationStatus = require('../config/invitation.status.js');
+const treeTypes = require('../config/tree.type.js');
 
 module.exports = server => {
   server.vars = {
     config: {
       clientStatus,
-      invitationStatus
+      invitationStatus,
+      treeTypes
     },
     const: {
     }
