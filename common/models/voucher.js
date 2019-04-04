acc23e86f982a50131fca87b1fb27c43f3a60b95
MrWooJ <Hurricanc@gmail.com>
2019-04-04 18:58:26 +0430

    Validate Voucher Properties e.g. Status & Type

--- common/models/voucher.js
+++ common/models/voucher.js
@@ -1,3 +1,19 @@
+let app = require('../../server/server');
+
 module.exports = async Voucher => {
+
+  let typeList = [];
+  for (let key in app.vars.config.voucherType) {
+    typeList.push(app.vars.config.voucherType[key]);
+	}
+
+	Voucher.validatesInclusionOf('type', { in: typeList });
+
+  let statusList = [];
+  for (let key in app.vars.config.voucherStatus) {
+    statusList.push(app.vars.config.voucherStatus[key]);
+	}
+
+	Voucher.validatesInclusionOf('status', { in: statusList });
 
 };
