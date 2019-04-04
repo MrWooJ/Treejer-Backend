f12ba344d415655f46a1cd139510649758d3341c
MrWooJ <Hurricanc@gmail.com>
2019-04-04 14:56:24 +0430

    Implement Change Status Function for Ivitation Model

---
+++ common/invitation/changeStatus.js
@@ -0,0 +1,59 @@
+const utility = rootRequire('helper/utility');
+let createError = require('http-errors');
+
+let app = rootRequire('server/server');
+
+module.exports = async Invitation => {
+
+  const vars = app.vars;
+  
+  Invitation.changeStatus = async (invitationCode, newStatus) => {
+    let invitationModel = 
+      await Invitation.fetchModel(invitationCode.toString());
+
+    if (!vars.config.invitationStatus[newStatus]) {
+      throw createError(404, 
+        'Error! The provided status code is not defined.');
+    }
+
+    let updatedInvitationModel = await invitationModel.updateAttributes({
+      status: newStatus,
+      lastUpdate: utility.getUnixTimeStamp()
+    });
+
+    return updatedInvitationModel;
+  };
+
+  Invitation.changeStatus = 
+    utility.wrapper(Invitation.changeStatus);
+
+  Invitation.remoteMethod('changeStatus', {
+    description: 'Change Status of the Invitation Code',
+    accepts: [{
+      arg: 'invitationCode',
+      type: 'string',
+      required: true,
+      http: {
+        source: 'path'
+      }
+    }, {
+      arg: 'newStatus',
+      type: 'string',
+      required: true,
+      http: {
+        source: 'path'
+      }
+    }],
+    http: {
+      path: '/:invitationCode/changeStatus/:newStatus',
+      verb: 'POST',
+      status: 200,
+      errorStatus: 400
+    },
+    returns: {
+      type: 'object',
+      root: true
+    }
+  });
+
+};
