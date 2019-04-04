aec118be3277552877434047653c7a5212b8ba9e
MrWooJ <Hurricanc@gmail.com>
2019-04-04 18:35:34 +0430

    Validate Receipt Properties e.g. Status & Type

--- common/models/receipt.js
+++ common/models/receipt.js
@@ -1,3 +1,19 @@
+let app = require('../../server/server');
+
 module.exports = async Receipt => {
+
+  let typeList = [];
+  for (let key in app.vars.config.receiptType) {
+    typeList.push(app.vars.config.receiptType[key]);
+	}
+
+	Receipt.validatesInclusionOf('type', { in: typeList });
+
+  let statusList = [];
+  for (let key in app.vars.config.receiptStatus) {
+    statusList.push(app.vars.config.receiptStatus[key]);
+	}
+
+	Receipt.validatesInclusionOf('status', { in: statusList });
 
 };
