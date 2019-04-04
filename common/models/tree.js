ef250c2bfe8439b2400a05533a953d6d0c6f98ab
MrWooJ <Hurricanc@gmail.com>
2019-04-04 18:15:14 +0430

    Validate Tree Properties e.g. Status & Type

--- common/models/tree.js
+++ common/models/tree.js
@@ -1,3 +1,19 @@
+let app = require('../../server/server');
+
 module.exports = async Tree => {
+
+  let typeList = [];
+  for (let key in app.vars.config.treeType) {
+    typeList.push(app.vars.config.treeType[key]);
+	}
+
+	Tree.validatesInclusionOf('type', { in: typeList });
+
+  let statusList = [];
+  for (let key in app.vars.config.treeStatus) {
+    statusList.push(app.vars.config.treeStatus[key]);
+	}
+
+	Tree.validatesInclusionOf('status', { in: statusList });
 
 };
