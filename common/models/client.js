65cb99414b85cc5d750cd2071198befa088a9a49
MrWooJ <Hurricanc@gmail.com>
2019-04-16 11:21:46 +0430

    Fix a Bug in Status Property Validation for Client Model

--- common/models/client.js
+++ common/models/client.js
@@ -3,8 +3,8 @@ let app = require('../../server/server');
 module.exports = async Client => {
 
   let statusList = [];
-  for (let key in app.vars.config.invitationStatus) {
-    statusList.push(app.vars.config.invitationStatus[key]);
+  for (let key in app.vars.config.clientStatus) {
+    statusList.push(app.vars.config.clientStatus[key]);
 	}
 
 	Client.validatesInclusionOf('status', { in: statusList });
