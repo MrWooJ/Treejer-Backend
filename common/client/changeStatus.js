5f1044230f86081413958a3e555682e629d4a6ae
MrWooJ <Hurricanc@gmail.com>
2019-04-16 12:11:06 +0430

    Prevent Client from Double Spend an Invitation Link if He/She was Verified

--- common/client/changeStatus.js
+++ common/client/changeStatus.js
@@ -1,4 +1,5 @@
 const utility = rootRequire('helper/utility');
+let createError = require('http-errors');
 
 let app = rootRequire('server/server');
 
@@ -8,6 +9,10 @@ module.exports = async Client => {
   
   Client.changeStatus = async (clientId, invitationCode) => {
     let clientModel = await Client.fetchModel(clientId.toString());
+
+    if (clientModel.status !== vars.config.clientStatus.waitList) {
+      throw createError(400, 'Error! Client is alreay invited to the treejer.');
+    }
 
     let Invitation = app.models.invitation;
     await Invitation.increaseUsage(invitationCode.toString());
