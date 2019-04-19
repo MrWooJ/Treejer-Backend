61324b8037c193f11f2fb34351dfa612b058e0a4
MrWooJ <Hurricanc@gmail.com>
2019-04-19 23:59:12 +0430

    Add New Properties to Tree Type Sample Data

--- config/tree.type.js
+++ config/tree.type.js
@@ -1,14 +1,18 @@
 module.exports = {
   treeNo1: {
     type: 'Populus',
+    scientificName: 'A Name',
     price: 100,
+    geoLocation: '(x1, y1)',
     region: 'Planet',
     drive: 'A Site',
     o2RatePerDay: 15
   },
   treeNo2: {
     type: 'Quercus',
+    scientificName: 'B Name',
     price: 100,
+    geoLocation: '(x2, y2)',
     region: 'Planet',
     drive: 'B Site',
     o2RatePerDay: 25
