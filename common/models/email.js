b77d7e44b906c6f75df754a4ca1131494daff9ba
MrWooJ <Hurricanc@gmail.com>
2019-04-19 23:41:43 +0430

    Add Finalized Gift Receipt Email Sender Module

--- common/models/email.js
+++ common/models/email.js
@@ -3,5 +3,6 @@ module.exports = async EmailSender => {
   require('../email/sendEmail')(EmailSender);
   require('../email/sendFinalizedBusinessReceiptEmail')(EmailSender);
   require('../email/sendFinalizedClientReceiptEmail')(EmailSender);
+  require('../email/sendFinalizedGiftReceiptEmail')(EmailSender);
 
 };
