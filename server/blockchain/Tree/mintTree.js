4e2b0086a0ffcf7cf9d6ac5e68f37e8a8408dc53
MrWooJ <Hurricanc@gmail.com>
2019-04-20 21:30:06 +0430

    Implement Mint a Tree based on Provided Data from Smart Contract

---
+++ server/blockchain/Tree/mintTree.js
@@ -0,0 +1,9 @@
+const getTreeContract = require('./getTreeContract');
+
+module.exports = async (treeId, clientId, createdDate, 
+  lastUpdate, procedure, status, planter, conserver, ranger) => {
+  const Tree = await getTreeContract();
+  const result = await Tree.mintTree(treeId, clientId, createdDate, 
+    lastUpdate, procedure, status, planter, conserver, ranger).send();
+  return result;
+};
