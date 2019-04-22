5a9c080ce4e58f52b1843535ccf87bdd0865b84d
MrWooJ <Hurricanc@gmail.com>
2019-04-22 10:47:41 +0430

    Add New Tree Types and Modify Previous Ones

--- config/tree.type.js
+++ config/tree.type.js
@@ -1,22 +1,32 @@
 module.exports = {
   treeNo1: {
-    type: 'Populus',
-    scientificName: 'A Name',
-    price: 100,
-    geoLocation: '(x1, y1)',
-    region: 'Planet',
-    drive: 'A Site',
+    type: 'Oak',
+    scientificName: 'Quercus Brantii',
+    price: 5,
+    geoLocation: '(37.3985523, 55.7814630)',
+    region: 'Golestan, Iran',
+    drive: 'Tangrah Village',
     age: 1,
-    o2RatePerDay: 15
+    o2RatePerDay: 2.5
   },
   treeNo2: {
-    type: 'Quercus',
-    scientificName: 'B Name',
-    price: 100,
-    geoLocation: '(x2, y2)',
-    region: 'Planet',
-    drive: 'B Site',
-    age: 2,
-    o2RatePerDay: 25
+    type: 'Jujube',
+    scientificName: 'Ziziphus Jujuba',
+    price: 7,
+    geoLocation: '(27.5277132, 57.8651371)',
+    region: 'Kerman, Iran',
+    drive: 'Ghalehganj Village',
+    age: 1,
+    o2RatePerDay: 2.1
+  },
+  treeNo3: {
+    type: 'Jujube',
+    scientificName: 'Ziziphus Jujuba',
+    price: 8,
+    geoLocation: '(25.5960424, 61.1010008)',
+    region: 'Sistan and Baluchestan, Iran',
+    drive: 'Seydbar Village',
+    age: 1,
+    o2RatePerDay: 2.3
   }
 };
